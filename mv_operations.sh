c3879062783045f7ee0e5c65d7815752ddc80054
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-09-01 11:24:48 +0530

    Update mv_operations.sh

--- mv_operations.sh
+++ mv_operations.sh
@@ -1,41 +1,24 @@
 #!/bin/bash
 
-# ============================================================
-# Linux mv Command Assignment
-# ============================================================
-#
-# Student Name:
-# Register Number:
-#
-# Complete the TODO sections.
-# Use the mv command for all operations.
-# ============================================================
-
 case "$1" in
-
     move)
-        # Task 1: Move file1.txt into documents/
-        # TODO: Write your mv command here
+        mv file1.txt documents/
         ;;
 
     rename)
-        # Task 2: Rename file1.txt to file2.txt
-        # TODO: Write your mv command here
+        mv file1.txt file2.txt
         ;;
 
     multiple)
-        # Task 3: Move file1.txt and file2.txt into documents/
-        # TODO: Write your mv command here
+        mv file1.txt file2.txt documents/
         ;;
 
     directory)
-        # Task 4: Move source_dir into documents/
-        # TODO: Write your mv command here
+        mv source_dir documents/
         ;;
 
     *)
         echo "Usage: $0 {move|rename|multiple|directory}"
         exit 1
         ;;
-
 esac
