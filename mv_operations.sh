19f8f888ae5beede537a1c35cdc19e1aa9d3dc9a
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-09-01 10:49:47 +0530

    Create mv_operations.sh

---
+++ mv_operations.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+Linux mv Command Assignment
+Student Name:
+Register Number:
+--------------------------------------------------
+Task 1: Move file1.txt into documents/
+--------------------------------------------------
+TODO: Write your mv command here
+--------------------------------------------------
+Task 2: Rename file1.txt to file2.txt
+--------------------------------------------------
+TODO: Write your mv command here
+--------------------------------------------------
+Task 3: Move file1.txt and file2.txt into documents/
+--------------------------------------------------
+TODO: Write your mv command here
+--------------------------------------------------
+Task 4: Move source_dir into documents/
+--------------------------------------------------
+TODO: Write your mv command here
